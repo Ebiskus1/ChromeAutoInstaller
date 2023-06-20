b873201b0e088a271411fe53e88b7f52e9c1b397
Ebiskus1 <82061506+Ebiskus1@users.noreply.github.com>
2023-06-20 22:47:31 +0300

    GoogleChromeInstaller.cmd

--- GoogleChromeInstaller.cmd
+++ GoogleChromeInstaller.cmd
@@ -5,9 +5,9 @@ setlocal EnableDelayedExpansion
 set DOWNLOAD_URL=https://dl.google.com/chrome/install/standalone/ChromeStandaloneSetup.exe
 set DOWNLOAD_FILE=%USERPROFILE%\Desktop\ChromeStandaloneSetup.exe
 
-powershell -command Invoke-WebRequest %DOWNLOAD_URL% -OutFile “%DOWNLOAD_FILE%”
+powershell -command Invoke-WebRequest %DOWNLOAD_URL% -OutFile "%DOWNLOAD_FILE%"
 
-if NOT EXIST “%DOWNLOAD_FILE%” (
+if NOT EXIST "%DOWNLOAD_FILE%" (
 echo Error downloading installation file.
 exit /b 1
 )
@@ -17,7 +17,7 @@ echo Google Chrome installation file downloaded.
 echo Installing Google Chrome…
 set SILENT_SWITCHES=/install /silent /norestart
 
-start /wait “” “%DOWNLOAD_FILE%” %SILENT_SWITCHES%
+start /wait "" "%DOWNLOAD_FILE%" %SILENT_SWITCHES%
 
 if %errorlevel% neq 0 (
 echo Error installing Google Chrome.
